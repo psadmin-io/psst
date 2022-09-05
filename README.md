0f08a3568b37c55e601638aebee1e7dd31c8e290
Kyle Benson <kyle@psadmin.io>
2022-09-05 11:32:54 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # psst
 PeopleSoft Secrets Tool
+
+# Installing
+```
+cd psst
+pip install
+```
+
+# Running directly
+```
+cd psst
+python3 -m psst
+```
+
+# Setting up Development
+```
+pip install virtualenv 
+
+cd psst
+virtualenv venv
+. venv/bin/activate
+
+pip install --editable .
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text()
+HISTORY = (HERE / "HISTORY.md").read_text()
+
+setup_args = dict(
+    name='psst',
+    version='0.0.0',
+    description='psst',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    url='https://github.com/psadmin-io/psst',
+    
+    author='psadmin.io',
+    author_email='info@psadmin.io',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=find_packages(),
+    keywords=['PeopleSoft', 'PeopleTools', 'OCI', 'Oracle Cloud Infrastructure', 'Oracle Cloud'],    
+    include_package_data=True,
+    install_requires=["Click"],
+    entry_points={
+        "console_scripts": [
+            "psst=psst.psst:cli",
+        ]
+    },
+)
+
+if __name__ == '__main__':
+    setup(**setup_args) 
