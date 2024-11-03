992d6709b040ff71b8e6e1a7e8297a19da523318
Kyle Benson <kyle@psadmin.io>
2024-11-03 15:03:38 -0600

    Added `vault update`

--- psst/psst.py
+++ psst/psst.py
@@ -98,3 +98,44 @@ def generate(type, name, compartment_id, region, secrets_list, prefix, suffix):
             dict[prefix + s + suffix] = eval("psst.secrets." + secrets_list + "." + s + ".generate()")
 
         vault = psst.vault.oci.create(ocicfg, name, compartment_id, dict)
+
+@vault.command("update")
+@click.option('-t','--type',
+              default="oci",
+              show_default=True,
+              help="The type of vault to create")
+@click.option('-v','--vault', required=True,
+              help="Vault ID (OCID for OCI, etc)")
+@click.option('-k','--key', required=True,
+              help="Key ID (OCID for OCI, etc)")
+@click.option('-c','--compartment-id', required=True,
+              help="Set the compartment for the vault, key and secrets")
+@click.option('-r','--region',
+              help="Set the region, overriding the default cloud configuration value")
+@click.option('-l', '--secrets-list', 
+              default="base",
+              show_default=True,
+              help="The secrets list to generate [base,pcm,oci]")
+@click.option('-p','--prefix',
+              default="",
+              help="Add a prefix to the secret names")
+@click.option('-s','--suffix',
+              default="",
+              help="Add a suffix to the secret names")
+def update(type, vault, key, compartment_id, region, secrets_list, prefix, suffix):
+    """Update a vault. Currently defaults a lot, including generated secrets..."""
+
+    if type == "oci":
+        ocicfg = psst.vault.oci.config(region)  # set region local here vs passing to function?        
+        
+        dict = {}
+        secrets = []
+
+        list_module = eval("psst.secrets." + secrets_list )
+        for module in getmembers(list_module, ismodule):
+            secrets.append(module[0])
+
+        for s in secrets:
+            dict[prefix + s + suffix] = eval("psst.secrets." + secrets_list + "." + s + ".generate()")
+
+        vault = psst.vault.oci.update(ocicfg, vault, key, compartment_id, dict)
